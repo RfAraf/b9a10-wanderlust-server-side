2d71796649688ae498739ce64ec6f9b6e80c3e18
rfaraf <mohsinarfat53@gamil.com>
2024-04-27 20:31:06 +0600

    implemented environment variable to hide mongodb credentials

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const cors = require("cors");
+require("dotenv").config();
 const { MongoClient, ServerApiVersion } = require("mongodb");
 const app = express();
 const port = process.env.PORT || 5000;
@@ -11,8 +12,7 @@ app.use(express.json());
 // tourismGuy
 // TBOTFk9WsE5V28u8
 
-const uri =
-  "mongodb+srv://tourismGuy:TBOTFk9WsE5V28u8@cluster0.0xqywot.mongodb.net/?retryWrites=true&w=majority";
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.0xqywot.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
