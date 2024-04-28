a35be3a4f663871107b0fb0ca11a69b711a52391
rfaraf <mohsinarfat53@gamil.com>
2024-04-28 08:02:50 +0600

    read certain tourist spot in database as API

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 require("dotenv").config();
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const app = express();
 const port = process.env.PORT || 5000;
 
@@ -36,6 +36,14 @@ async function run() {
       const cursor = touristSpotCollection.find();
       const result = await cursor.toArray();
       res.send(result);
+    });
+
+    // read certain tourist spot in database as API
+    app.get("/touristSpots/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const touristSpot = await touristSpotCollection.findOne(query);
+      res.send(touristSpot);
     });
 
     // create a tourist spot
